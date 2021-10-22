190706f7a855b639b87f232064cd4acf1fc5cf9b
Maksym Shokin <problaze7@gmail.com>
2021-10-22 12:08:32 +0300

    init2

--- main.go
+++ main.go
@@ -2,54 +2,72 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"math/rand"
+	"time"
 )
 
+var source = rand.NewSource(time.Now().Unix())
+var randN = rand.New(source)
+
 func main() {
-	greet()
-	storeData("This is some dummy data!", "dummy-data.txt")
+	x := generateValue()
+	y := generateValue()
 
-	channel := make(chan int)
+	sum := x + y
 
-
-	go storeMoreData(50000, "50000_1.txt", channel)
-	go storeMoreData(50000, "50000_2.txt", channel)
-
-	<-channel
-	<-channel
+	fmt.Println(sum)
 }
 
-func greet() {
-	fmt.Println("Hi there!")
+func generateValue() int {
+	sleepTime := randN.Intn(3)
+	time.Sleep(time.Duration(sleepTime) * time.Second)
+
+	return randN.Intn(10)
 }
 
-func storeData(storableText string, fileName string) {
-	file, err := os.OpenFile(fileName,
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
-		0666,
-	)
+// func main() {
+// 	greet()
+// 	storeData("This is some dummy data!", "dummy-data.txt")
 
-	if err != nil {
-		fmt.Println("Creating the file failed. Exiting.")
-		return
-	}
+// 	channel := make(chan int)
 
-	defer file.Close()
+// 	go storeMoreData(50000, "50000_1.txt", channel)
+// 	go storeMoreData(50000, "50000_2.txt", channel)
 
-	_, err = file.WriteString(storableText)
+// 	<-channel
+// 	<-channel
+// }
 
-	if err != nil {
-		fmt.Println("Writing to the file failed.")
-	}
-}
+// func greet() {
+// 	fmt.Println("Hi there!")
+// }
 
-func storeMoreData(lines int, fileName string, c chan int) {
-	for i := 0; i < lines; i++ {
-		text := fmt.Sprintf("Line %v - Dummy Data\n", i)
-		storeData(text, fileName)
-	}
+// func storeData(storableText string, fileName string) {
+// 	file, err := os.OpenFile(fileName,
+// 		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
+// 		0666,
+// 	)
 
-	fmt.Printf("-- Done storing %v lines of text --\n", lines)
+// 	if err != nil {
+// 		fmt.Println("Creating the file failed. Exiting.")
+// 		return
+// 	}
 
-	c <- 1
-}
+// 	defer file.Close()
+
+// 	_, err = file.WriteString(storableText)
+
+// 	if err != nil {
+// 		fmt.Println("Writing to the file failed.")
+// 	}
+// }
+
+// func storeMoreData(lines int, fileName string, c chan int) {
+// 	for i := 0; i < lines; i++ {
+// 		text := fmt.Sprintf("Line %v - Dummy Data\n", i)
+// 		storeData(text, fileName)
+// 	}
+
+// 	fmt.Printf("-- Done storing %v lines of text --\n", lines)
+// 	c <- 1
+// }
